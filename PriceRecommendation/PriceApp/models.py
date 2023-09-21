8b57b92e9b371d0decdc4859fff2e1731c1c1380
Marah Ashayer <marahashayer317@gmail.com>
2023-09-21 14:35:03 +0300

    Login & Reg Backend

--- PriceRecommendation/PriceApp/models.py
+++ PriceRecommendation/PriceApp/models.py
@@ -1,3 +1,32 @@
+from email.policy import default
 from django.db import models
+import re
 
-# Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        errors = {}
+        if len(postData['firstname']) < 2:
+            errors["firstname"] = "User firstname should be at least 2 characters"
+        if len(postData['lastname']) < 2:
+            errors["lastname"] = "User lastname should be at least 2 characters"
+        if not EMAIL_REGEX.match(postData['email']):                
+            errors['email'] = "Invalid email address!"
+        if len(postData['password']) < 8:
+            errors["password"] = "User password should be at least 8 characters"
+        if postData['confirm_password'] != postData['password']:
+            errors['confirm_password']="password not match"
+        return errors
+
+class User(models.Model):
+    firstname = models.CharField(max_length=45)
+    lastname=models.CharField(max_length=45)
+    email=models.EmailField(max_length=45)
+    password=models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects=UserManager()
+
+
+
+
